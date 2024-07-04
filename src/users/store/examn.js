983840363a93348e4f93852e9d9ce16a45cb735c
diana <dianaclabel08@gmail.com>
2024-07-04 00:00:12 -0500

    feat: changes

---
+++ src/users/store/examn.js
@@ -0,0 +1,18 @@
+// for (var i = 0; i < 10; i++) {
+//   var count = 0;
+//   for (var j = 0; j < Math.floor(Math.random() * 10); j++) {
+//     count++;
+//   }
+//   console.log(count);
+// }
+
+let nombre = "Pedro";
+
+function mostrarNombre(nombre) {
+  if (nombre !== undefined) {
+    nombre = "Tu nombre es" + "" + nombre;
+  }
+}
+
+mostrarNombre(" Juan");
+console.log(nombre);
---
+++ src/users/store/users-store.js
@@ -0,0 +1,29 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page";
+
+const state = {
+  currentPage: 0,
+  users: [],
+};
+
+const loadNextPage = async () => {
+  await loadUsersByPage(state.currentPage + 1);
+};
+
+const loadPreviousPage = async () => {};
+//TODO implementar
+const onUserChanged = async () => {
+  throw new Error("No implementado");
+};
+
+const reloadPage = async () => {
+  throw new Error("No implementado");
+};
+
+export default {
+  loadNextPage,
+  loadPreviousPage,
+  onUserChanged,
+  reloadPage,
+  getUser: () => [...state.users],
+  getCurrentPage: () => state.currentPage,
+};
