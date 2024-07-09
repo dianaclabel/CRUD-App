981868317e69b3c9febeeab48f20465e7cd2b718
diana <dianaclabel08@gmail.com>
2024-07-09 02:00:31 -0500

    feat: Add button float

---
+++ src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,18 @@
+import "./render-add-button.css";
+
+/**
+ *
+ * @param { HTMLDivElement } element
+ * @param { () => void } callback
+ */
+export const renderAddButton = (element) => {
+  const fabButton = document.createElement("button");
+  fabButton.innerText = "+";
+  fabButton.classList.add("fab-button");
+
+  element.append(fabButton);
+
+  fabButton.addEventListener("click", () => {
+    throw Error("no implementado");
+  });
+};
